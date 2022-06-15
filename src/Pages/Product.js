3e78f156137e89f8f4c2cca4e4f87e33b8f41d29
abbasmojarrad <abbasmojarrad7@gmail.com>
2022-06-15 13:41:09 +0430

    add css to ProductDetails , fix  Product component

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,22 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 //css
 import styles from "../component/css/product.module.css";
 //component
 import ProductItems from "../component/ProductItems";
-import loader from '../assets/loader.gif'
+import loader from "../assets/loader.gif";
 //context
 import { ProductContext } from "../context/ProductContextProvider";
 const Product = () => {
   const products = useContext(ProductContext);
+  const [search, setSearch] = useState("");
+  const changeHandler = (e) => setSearch(e.target.value);
+  const filterProduct = products.filter((product) =>
+    product.title.toLowerCase().startsWith(search.toLowerCase())
+  );
   return (
-    <div className={styles.container}>
-      {!products.length ? (
-        <div><img style={{width:"120%"}} src={loader} alt="" /></div>
-      ) : (
-        products.map((product) => {
-          return <ProductItems key={product.id} data={product} />;
-        })
-      )}
+    <div>
+      <input
+        type="text"
+        name="product"
+        value={search}
+        onChange={changeHandler}
+      />
+      <div className={styles.container}>
+        {!products.length ? (
+          <div>
+            <img style={{ width: "120%" }} src={loader} alt="" />
+          </div>
+        ) : (
+          filterProduct.map((product) => {
+            return <ProductItems key={product.id} data={product} />;
+          })
+        )}
+      </div>
     </div>
   );
 };
--- src/component/ProductDetails.js
+++ src/component/ProductDetails.js
@@ -5,6 +5,11 @@ import { ProductContext } from "../context/ProductContextProvider";
 import { CartContext } from "../context/CartContextProvider";
 //helper function
 import { hasProduct, ProductNumber } from "../helper/functions";
+//css
+import styles from "./css/productdetails.module.css";
+import { HiTrash } from "react-icons/hi";
+//component
+import loader from "../assets/loader.gif";
 
 const ProductDetails = (props) => {
   const products = useContext(ProductContext);
@@ -18,44 +23,74 @@ const ProductDetails = (props) => {
     }
   });
   return (
-    <div>
-      <img style={{ width: "200px" }} src={product?.image} alt="" />
-      <h3>{product?.category}</h3>
-      <h3>{product?.title}</h3>
-      <p>{product?.price}$</p>
-      <p>{product?.description}</p>
-      <div>
-        <Link to="/product">back to shop</Link>
-        <div>
-          {hasProduct(state.cartList, product?.id) ? (
-            <button
-              onClick={() => dispatch({ type: "INCREASE", payload: product })}
-            >
-              +
-            </button>
-          ) : (
-            <button onClick={() => dispatch({ type: "ADD", payload: product })}>
-              add to cart
-            </button>
-          )}
-          <span>{ProductNumber(state.cartList, product?.id)}</span>
-          {ProductNumber(state.cartList, product?.id) === 1 && (
-            <button
-              onClick={() => dispatch({ type: "REMOVE", payload: product })}
-            >
-              remove
-            </button>
-          )}
-          {ProductNumber(state.cartList, product?.id) > 1 && (
-            <button
-              onClick={() => dispatch({ type: "DECREASE", payload: product })}
-            >
-              -
-            </button>
-          )}
-        </div>
+    <>
+      <Link to="/product">back to shop</Link>
+      <div className={styles.container}>
+        {!products.length ? (
+          <div>
+            <img style={{ width: "120%" }} src={loader} alt="" />
+          </div>
+        ) : (
+          <div className={styles.product__container}>
+            <div className={styles.image__container}>
+              <img src={product?.image} alt="" />
+            </div>
+            <div className={styles.details}>
+              <h3 className={styles.title}>{product?.title}</h3>
+              <h3 className={styles.category}>{product?.category}</h3>
+              <p className={styles.description}>{product?.description}</p>
+              <p className={styles.price}>{product?.price}$</p>
+              <div className={styles.buttons}>
+                <div>
+                  {hasProduct(state.cartList, product?.id) ? (
+                    <button
+                      className={styles.increase__button}
+                      onClick={() =>
+                        dispatch({ type: "INCREASE", payload: product })
+                      }
+                    >
+                      +
+                    </button>
+                  ) : (
+                    <button
+                      className={styles.add__button}
+                      onClick={() =>
+                        dispatch({ type: "ADD", payload: product })
+                      }
+                    >
+                      add to cart
+                    </button>
+                  )}
+                  <span className={styles.product__number}>
+                    {ProductNumber(state.cartList, product?.id)}
+                  </span>
+                  {ProductNumber(state.cartList, product?.id) === 1 && (
+                    <button
+                      className={styles.remove}
+                      onClick={() =>
+                        dispatch({ type: "REMOVE", payload: product })
+                      }
+                    >
+                      <HiTrash />
+                    </button>
+                  )}
+                  {ProductNumber(state.cartList, product?.id) > 1 && (
+                    <button
+                      className={styles.decrease__button}
+                      onClick={() =>
+                        dispatch({ type: "DECREASE", payload: product })
+                      }
+                    >
+                      -
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
